85cda31bf05ce95845afe691722022da4f8f75ca
rfargetton <romaric.fargetton@gmail.com>
2018-01-02 10:43:17 +0100

    the loop style

--- index.php
+++ index.php
@@ -11,27 +11,18 @@
 			<?php say_hello(); ?>
 		</h1>
 
-    <?php if(have_posts()):
-      $latest_posts = get_posts(array('numberposts'=>2));
-    ?>
-      <div class="posts">
-        <?php foreach ($latest_posts as $post): setup_postdata( $post ); ?>
+    <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
 
           <div class="post">
             <a href= "<?php the_permalink(); ?>">
               <h2><?php the_title(); ?></h2>
               <?php the_category() ?>
               <?php the_content(); ?>
-              
             </a>
 
           </div>
 
-        <?php endforeach; ?>
-
-
-      </div>
-    <?php  endif; ?>
+        <?php endwhile; endif; ?>
 
 	</body>
 </html>
