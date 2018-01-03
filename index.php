a71ab74c8a479dafece6d7792041eced312abd1a
rfargetton <romaric.fargetton@gmail.com>
2018-01-03 16:28:55 +0100

    code cleanup on index.php

--- index.php
+++ index.php
@@ -11,16 +11,17 @@
 			<?php say_hello(); ?>
 		</h1>
 
-    <?php if(have_posts()) : ?>
+    <?php
+      $recent_posts = new WP_Query(array('category_name' => 'travaux', 'posts_per_page' => 8 ));
+      if($recent_posts->have_posts()) : ?>
 
       <div class="posts">
-        <?php while(have_posts()) : the_post(); ?>
+        <?php while($recent_posts->have_posts()) : $recent_posts->the_post(); ?>
 
             <div id="<?php the_title(); ?>" class="project-thumbnail">
               <a href= "<?php the_permalink(); ?>">
                 <h2><?php the_title(); ?></h2>
                 <p><?php the_field('date'); ?></p>
-                <?php the_content(); ?>
               </a>
 
             </div>
