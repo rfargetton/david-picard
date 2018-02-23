846fd8c1486dd443b373aeee7898f92e4fe65892
rfargetton <romaric.fargetton@gmail.com>
2018-02-23 16:42:18 +0100

    merged the-loop branche

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,29 +28,29 @@ document.getElementById("nav").addEventListener("click", function(){
 
 });
 
-// function masonry(grid, gridCell, gridGutter, dGridCol, tGridCol, mGridCol) {
-//   var g = document.querySelector(grid),
-//       gc = document.querySelectorAll(gridCell),
-//       gcLength = gc.length,
-//       gHeight = 0,
-//       i;
-//
-//   for(i=0; i<gcLength; ++i) {
-//     gHeight+=gc[i].offsetHeight+parseInt(gridGutter);
-//   }
-//
-//   if (window.screen.width >= 1024) {
-//     g.style.height = gHeight/dGridCol + (gHeight/dGridCol)/gcLength + "px";
-//   } else if (window.screen.width < 1024 && window.screen.width >= 768) {
-//     g.style.height = gHeight/tGridCol + gHeight/(gcLength+1) + "px";
-//   } else {
-//     g.style.height = gHeight/mGridCol + gHeight/(gcLength+1) + "px";
-//   }
-//
-// }
-//
-// ["resize", "load"].forEach(function(event) {
-//   window.addEventListener(event, function() {
-//     masonry(".grid", ".cell", 0, 2, 2, 1);
-//   });
-// });
+function masonry(grid, gridCell, gridGutter, dGridCol, tGridCol, mGridCol) {
+  var g = document.querySelector(grid),
+      gc = document.querySelectorAll(gridCell),
+      gcLength = gc.length,
+      gHeight = 0,
+      i;
+
+  for(i=0; i<gcLength; ++i) {
+    gHeight+=gc[i].offsetHeight+parseInt(gridGutter);
+  }
+
+  if (window.screen.width >= 1024) {
+    g.style.height = gHeight/dGridCol + (gHeight/dGridCol)/gcLength + "px";
+  } else if (window.screen.width < 1024 && window.screen.width >= 768) {
+    g.style.height = gHeight/tGridCol + gHeight/(gcLength+1) + "px";
+  } else {
+    g.style.height = gHeight/mGridCol + gHeight/(gcLength+1) + "px";
+  }
+
+}
+
+["resize", "load"].forEach(function(event) {
+  window.addEventListener(event, function() {
+    masonry(".grid", ".cell", 0, 2, 2, 1);
+  });
+});
