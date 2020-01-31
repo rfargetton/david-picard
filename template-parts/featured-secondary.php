ffa880b7c5596cba39a447b3615b07734317c345
Romaric Fargetton <romaric.fargetton@gmail.com>
2020-01-31 11:33:32 +0100

    added special fields for text posts

--- template-parts/featured-secondary.php
+++ template-parts/featured-secondary.php
@@ -5,12 +5,22 @@
 
     <h1><?php the_title(); ?></h1>
 
+    <?php if (in_category('textes')) : ?>
+      <p>
+        <?php the_field('publication') ?></br>
+        par <?php the_field('auteur') ?>
+      </p>
+    <?php endif; ?>
+
   </div>
 
   <div class="project-content">
 
     <div class="content">
       <?php the_content() ?>
+      <?php if (in_category('textes')) : ?>
+        <a class="download-link" href="<?php the_field('fichier'); ?>" target=_blank > &darr; Télécharger le texte</a>
+      <?php endif; ?>
     </div>
 
   </div>
